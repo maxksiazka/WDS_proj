46a81ab0435f6af3219e890f93c3c1a2a56a6a7f
max_ksiazka <maksymilian.ksiazka@gmail.com>
2026-06-09 19:43:49 +0200

    feat: multithread DataFusion, add mainpage.dox, change binary name

--- desktop_application/main.cpp
+++ desktop_application/main.cpp
@@ -1,17 +1,23 @@
 #include "SensorLink.hpp"
 #include <QApplication>
+#include <QThread>
 #include "MainWindow.hpp"
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
-    MainWindow w;
     SensorLink link;
+    link.start(12345, 5001);
+
+    QThread data_thread;
     DataFusionEngine data_engine;
+    data_engine.moveToThread(&data_thread);
+    QObject::connect(&data_thread, SIGNAL(finished()),&data_thread, SLOT(deleteLater()));
     data_engine.connectToSensorLink(&link);
+
+    MainWindow w;
     w.connectDataFusionEngineToPFD(&data_engine);
     w.connectDataFusionEngineToDCP(&data_engine);
     w.connectSensorLinkToMFD(&link);
     w.connectDataFusionEngineToMFD(&data_engine);
-    link.start(12345, 5001);
     w.show();
     return a.exec();
 }
